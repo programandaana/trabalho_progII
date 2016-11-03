bbb62a4aba4673536742d26049ecd93a4da6e051
Ana Prado <engflorestalana@hotmail.com>
2016-11-03 20:07:35 -0200

    Iniciando o projeto

---
+++ Conversor_moedas/src/GeradorGrafico.java
@@ -0,0 +1,13 @@
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+
+public class GeradorGrafico {
+    DefaultCategoryDataset ds = new DefaultCategoryDataset();
+    for 
+    JFreeChart grafico = ChartFactory.createLineChart("Variação do Dólar Americano", "Dia", 
+    "Valor", ds, PlotOrientation.VERTICAL, true, true, false);
+   }
